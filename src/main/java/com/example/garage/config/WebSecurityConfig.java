582a6e3798a3620d2a013e3585071ee08adf5969
saniayudin <olex.yudin@gmail.com>
2025-03-20 12:34:12 +0200

    Security

--- src/main/java/com/example/garage/config/WebSecurityConfig.java
+++ src/main/java/com/example/garage/config/WebSecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import java.net.http.HttpRequest;
+
 /**
  * Насетапить авторизацию для постман (не через form-data)
  */
@@ -32,44 +34,90 @@ public class WebSecurityConfig {
         this.userDetailsService = userDetailsService;
     }
 
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .authorizeHttpRequests((authorize) ->
+//                                authorize
+////                                .requestMatchers("/registration").permitAll()
+////                                .requestMatchers("/carservice/**").hasAnyAuthority("ADMIN", "USER")
+////                                .requestMatchers("/user**", "/users/**").hasAuthority("ADMIN")
+////                                .requestMatchers("/carservice/**").permitAll()
+////                                .anyRequest().authenticated()
+////                                .and()
+//                                        .anyRequest().permitAll()
+//
+//                ).formLogin(
+//                        form -> form
+//                                .loginPage("/login")
+//                                .loginProcessingUrl("/login")
+//                                .defaultSuccessUrl("/carservice/clients")
+//                                .permitAll()
+//                ).logout(
+//                        logout -> logout
+//                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//                                .permitAll()
+//                )
+//                .csrf().disable() // Отключаем CSRF, если это API или тестовый режим
+//                .headers().frameOptions().disable(); // Отключаем защиту от фреймов (если нужна)
+//
+//
+////                .formLogin(
+////                form -> form
+////                        .loginPage("/login")
+////                        .loginProcessingUrl("/login")
+////                        .defaultSuccessUrl("/carservice/clients")
+////                        .permitAll()
+////        ).logout(
+////                logout -> logout
+////                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+////                        .permitAll()
+//        return http.build();
+//    }
+
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authorize) ->
-                                authorize
-//                                .requestMatchers("/registration").permitAll()
-//                                .requestMatchers("/carservice/**").hasAnyAuthority("ADMIN", "USER")
-//                                .requestMatchers("/user**", "/users/**").hasAuthority("ADMIN")
-//                                .requestMatchers("/carservice/**").permitAll()
-//                                .anyRequest().authenticated()
-//                                .and()
-                                        .anyRequest().permitAll()
+                .authorizeHttpRequests()
+                .requestMatchers("/carservice/clients/add",
+                        "/carservice/clients/*/update",
+                        "/carservice/clients/*/delete",
+                        "/carservice/clients/*/cars/add",
+                        "/carservice/clients/*/cars/*/update",
+                        "/carservice/clients/*/cars/*/delete",
+                        "/carservice/clients/*/cars/*/service-history/*/add",
+                        "/carservice/clients/*/cars/*/service-history/*/update",
+                        "/carservice/clients/*/cars/*/service-history/*/delete")
+                .hasAnyAuthority("ADMIN")
+                .requestMatchers("/login",
+                        "/carservice/clients",
+                        "/carservice/clients/*/cars",
+                        "/carservice/clients/*/cars/*/service-history")
+                .hasAnyAuthority("ADMIN", "USER")
+                .and()
 
-                ).formLogin(
-                        form -> form
-                                .loginPage("/login")
-                                .loginProcessingUrl("/login")
-                                .defaultSuccessUrl("/carservice/clients")
-                                .permitAll()
-                ).logout(
-                        logout -> logout
-                                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                                .permitAll()
-                )
-                .csrf().disable() // Отключаем CSRF, если это API или тестовый режим
-                .headers().frameOptions().disable(); // Отключаем защиту от фреймов (если нужна)
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/carservice/clients")
+                .permitAll()
+
+                .and()
+                .csrf().disable()  // Отключаем CSRF, если это API или тестовый режим
+                .headers().frameOptions().disable();  // Отключаем защиту от фреймов (если нужна)
 
 
-//                .formLogin(
-//                form -> form
-//                        .loginPage("/login")
-//                        .loginProcessingUrl("/login")
-//                        .defaultSuccessUrl("/carservice/clients")
-//                        .permitAll()
-//        ).logout(
-//                logout -> logout
-//                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-//                        .permitAll()
+//        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//            http
+//                    .authorizeHttpRequests()
+//                    .anyRequest().permitAll() // Делаем все страницы доступными без авторизации
+//
+//                    .and()
+//                    .csrf().disable()  // Отключаем CSRF (если API)
+//                    .headers().frameOptions().disable();  // Отключаем защиту от фреймов (если нужно)
+
+//            return http.build();
+
         return http.build();
     }
 }
